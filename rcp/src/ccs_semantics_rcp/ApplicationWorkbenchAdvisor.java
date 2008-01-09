7125ac81bcf4fc9466839600d41e99c4e5a570a2
clemens <clemens@2e72368a-7642-0410-b8c5-9fb0f2bdb46c>
2008-01-09 21:32:20 +0000

    git-svn-id: https://hammacher.name/svn/ccs@22 2e72368a-7642-0410-b8c5-9fb0f2bdb46c

---
+++ rcp/src/ccs_semantics_rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package ccs_semantics_rcp;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "CCS_Semantics_RCP.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
