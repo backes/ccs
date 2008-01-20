5d642cf9132d329c342269b6d54a762c8acaf562
clemens <clemens@2e72368a-7642-0410-b8c5-9fb0f2bdb46c>
2008-01-20 02:09:41 +0000

    git-svn-id: https://hammacher.name/svn/ccs@72 2e72368a-7642-0410-b8c5-9fb0f2bdb46c

---
+++ plugin/src/de/unisb/cs/depend/ccs_sem/plugin/wizards/NewCCSProjectWizard.java
@@ -0,0 +1,25 @@
+package de.unisb.cs.depend.ccs_sem.plugin.wizards;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+
+
+public class NewCCSProjectWizard extends BasicNewProjectResourceWizard {
+
+    public NewCCSProjectWizard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void createPageControls(Composite pageContainer) {
+        // TODO Auto-generated method stub
+        super.createPageControls(pageContainer);
+    }
+
+    @Override
+    public boolean performFinish() {
+        // TODO Auto-generated method stub
+        return super.performFinish();
+    }
+}
