2190054fdd8e42f7c50da38452963befc39a317d
clemens <clemens@2e72368a-7642-0410-b8c5-9fb0f2bdb46c>
2008-01-20 23:47:23 +0000

    git-svn-id: https://hammacher.name/svn/ccs@78 2e72368a-7642-0410-b8c5-9fb0f2bdb46c

---
+++ core/src/de/unisb/cs/depend/ccs_sem/semantics/types/values/ParameterRefChannel.java
@@ -0,0 +1,34 @@
+package de.unisb.cs.depend.ccs_sem.semantics.types.values;
+
+import java.util.Map;
+
+import de.unisb.cs.depend.ccs_sem.semantics.types.Parameter;
+
+
+public class ParameterRefChannel extends ParameterReference implements Channel {
+
+    public ParameterRefChannel(Parameter param) {
+        super(param);
+    }
+
+    @Override
+    public Channel instantiate(Map<Parameter, Value> parameters) {
+        final Value newValue = super.instantiate(parameters);
+        if (newValue instanceof Channel)
+            return (Channel)newValue;
+        if (newValue instanceof ConstString) {
+            final ConstString str = (ConstString)newValue;
+            return new ConstStringChannel(str.getValue(), str.isNeedsQuotes());
+        }
+        if (newValue instanceof ParameterReference)
+            return new ParameterRefChannel(((ParameterReference)newValue).getParam());
+        assert false;
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode() + 29;
+    }
+
+}
