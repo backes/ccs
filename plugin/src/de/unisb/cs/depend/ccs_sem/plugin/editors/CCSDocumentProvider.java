11f8e3856dd95f6db9abcb1dec149717c05ca94c
clemens <clemens@2e72368a-7642-0410-b8c5-9fb0f2bdb46c>
2008-05-26 18:50:08 +0000

    next eclipse plugin release: can now open files that belong to no project in the workspace (but with limited functionality)
    git-svn-id: https://hammacher.name/svn/ccs@207 2e72368a-7642-0410-b8c5-9fb0f2bdb46c

--- plugin/src/de/unisb/cs/depend/ccs_sem/plugin/editors/CCSDocumentProvider.java
+++ plugin/src/de/unisb/cs/depend/ccs_sem/plugin/editors/CCSDocumentProvider.java
@@ -1,6 +1,15 @@
 package de.unisb.cs.depend.ccs_sem.plugin.editors;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
 
 public class CCSDocumentProvider extends FileDocumentProvider {
@@ -8,6 +17,32 @@ public class CCSDocumentProvider extends FileDocumentProvider {
     @Override
     protected IDocument createEmptyDocument() {
         return new CCSDocument();
+    }
+
+    @Override
+    protected boolean setDocumentContent(IDocument document,
+            IEditorInput editorInput, String encoding) throws CoreException {
+        if (editorInput instanceof IURIEditorInput) {
+            final URI uri = ((IURIEditorInput)editorInput).getURI();
+            final File file = new File(uri);
+            FileInputStream stream;
+            try {
+                stream = new FileInputStream(file);
+            } catch (final FileNotFoundException e) {
+                return false;
+            }
+            try {
+                setDocumentContent(document, stream, encoding);
+            } finally {
+                try {
+                    stream.close();
+                } catch (final IOException x) {
+                    // ignore
+                }
+            }
+            return true;
+        }
+        return super.setDocumentContent(document, editorInput, encoding);
     }
 
 }
